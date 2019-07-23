edea403884cd5f70707ef2d63ab55af2e6d93d52
Mariana <mary.kravets@gmail.com>
2019-07-23 09:35:21 -0400

    revert

--- app/src/main/java/com/example/test/OrgChartTestActivity.java
+++ app/src/main/java/com/example/test/OrgChartTestActivity.java
@@ -2,6 +2,8 @@ package com.example.test;
 
 import android.support.v7.app.AppCompatActivity;
 import com.example.test.list.implementation.obj.EmployeeJava;
+//or comment list.implementation and uncomment tree.implementation
+//import com.example.test.tree.implementation.EmployeeJava;
 
 public class OrgChartTestActivity extends AppCompatActivity {
 
